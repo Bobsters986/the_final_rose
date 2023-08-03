7b22761564a0af1bba1bf588e2c1cffdbdc15436
Bobby Luly <bobbyluly@gmail.com>
2023-08-03 16:01:59 -0600

    Test: create test for contestants#index

--- spec/features/bachelorette/show_spec.rb
+++ spec/features/bachelorette/show_spec.rb
@@ -26,5 +26,15 @@ RSpec.describe "Bachlorette Show Page", type: :feature do
 
     click_link("Contestants")
     expect(current_path).to eq(bachelorette_contestants_path(@bachelorette_1))
+    expect(page).to have_content("#{@bachelorette_1.name}'s Contestants")
+
+    within("#contestant") do
+      expect(page).to have_content(@contestant_1.name)
+      expect(page).to have_content(@contestant_2.name)
+      expect(page).to have_content(@contestant_3.name)
+
+      expect(page).to_not have_content(@contestant_4.name)
+      expect(page).to_not have_content(@contestant_5.name)
+    end
   end
 end
