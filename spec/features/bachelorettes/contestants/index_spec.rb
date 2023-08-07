9bc9a0930964fbb84122d0fea9078c028c485aaa
Bobby Luly <bobbyluly@gmail.com>
2023-08-07 14:58:23 -0600

    Add test for unique hometowns

--- spec/features/bachelorettes/contestants/index_spec.rb
+++ spec/features/bachelorettes/contestants/index_spec.rb
@@ -8,9 +8,11 @@ RSpec.describe "Bachlorette's Contestants Index Page", type: :feature do
 
     @contestant_1 = Contestant.create!(name: "Dustin Kendrick", age: 30, hometown: "Chicago, IL", bachelorette_id: @bachelorette_1.id)
     @contestant_2 = Contestant.create!(name: "Mike Johnson", age: 31, hometown: "San Antonio, TX", bachelorette_id: @bachelorette_1.id)
-    @contestant_3 = Contestant.create!(name: "Jed Wyatt", age: 25, hometown: "Knoxville, TN", bachelorette_id: @bachelorette_1.id)
-    @contestant_4 = Contestant.create!(name: "Tyler Cameron", age: 26, hometown: "Jupiter, FL", bachelorette_id: @bachelorette_2.id)
-    @contestant_5 = Contestant.create!(name: "Peter Weber", age: 27, hometown: "Westlake Village, CA", bachelorette_id: @bachelorette_2.id)
+    @contestant_3 = Contestant.create!(name: "Jed Wyatt", age: 25, hometown: "Centennial, CO", bachelorette_id: @bachelorette_1.id)
+    @contestant_4 = Contestant.create!(name: "John Doe", age: 30, hometown: "Chicago, IL", bachelorette_id: @bachelorette_1.id)
+    @contestant_5 = Contestant.create!(name: "Jane Doe", age: 30, hometown: "San Antonio, TX", bachelorette_id: @bachelorette_1.id)
+    @contestant_6 = Contestant.create!(name: "Tyler Cameron", age: 26, hometown: "Jupiter, FL", bachelorette_id: @bachelorette_2.id)
+    @contestant_7 = Contestant.create!(name: "Peter Weber", age: 27, hometown: "Westlake Village, CA", bachelorette_id: @bachelorette_2.id)
   end
 
   it "has a list of each contestant's info" do
@@ -31,8 +33,8 @@ RSpec.describe "Bachlorette's Contestants Index Page", type: :feature do
       expect(page).to have_content("Age: #{@contestant_3.age}")
       expect(page).to have_content("Hometown: #{@contestant_3.hometown}")
 
-      expect(page).to_not have_link(@contestant_4.name)
-      expect(page).to_not have_link(@contestant_5.name)
+      expect(page).to_not have_link(@contestant_6.name)
+      expect(page).to_not have_link(@contestant_7.name)
     end
   end
 
@@ -44,5 +46,11 @@ RSpec.describe "Bachlorette's Contestants Index Page", type: :feature do
     end
 
     expect(current_path).to eq(contestant_path(@contestant_1))
+  end
+
+  it "shows the contestants unique hometowns" do
+    visit bachelorette_contestants_path(@bachelorette_1)
+
+    expect(page).to have_content("These Contestants are from the following hometowns: Centennial, CO, Chicago, IL, San Antonio, TX,")
   end
 end
